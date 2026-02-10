bffbbab41922404a21c8e334576217bd7484a737
DenisKoriavets <d.korjavets@ukr.net>
2026-02-10 22:50:56 +0200

    feat: add ProjectDataModel class

---
+++ TaskManager.DataModels/ProjectDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KMA.TaskManager.Common.Enums;
+
+namespace TaskManager.DataModels
+{
+    public class ProjectDataModel
+    {
+        // id is read-only
+        public int Id { get; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ProjectType projectType { get; set; }
+    }
+}
