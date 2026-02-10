246dfb95206fdc8f036fcc4ef3e379744d7c4813
Artem <artemgritsenko2007@gmail.com>
2026-02-10 21:31:07 +0200

    init solution structure

---
+++ TaskManager.Common/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Common
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TaskManager.DataModels/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.DataModels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
