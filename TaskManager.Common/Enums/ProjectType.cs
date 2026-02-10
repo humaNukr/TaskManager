fd77a4428e960a08233e3c6c4714735fad06c64b
DenisKoriavets <d.korjavets@ukr.net>
2026-02-10 22:45:04 +0200

    feat: add ProjectType enum

---
+++ TaskManager.Common/Enums/ProjectType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.TaskManager.Common.Enums
+{
+    public enum ProjectType
+    {
+        Educational,
+        Work,
+        Personal,
+        Research,
+        Hobby,
+        Volunteer,
+        Other
+    }
+}
