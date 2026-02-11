49b8865348ca1332db2211a0ce143a129798cb1e
DenisKoriavets <d.korjavets@ukr.net>
2026-02-11 19:51:11 +0200

    feat: display project list using ProjectService

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,17 +1,29 @@
-﻿namespace TaskManager
+﻿using System.Text;
+using KMA.TaskManager.Services;
+
+namespace TaskManager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             while (true)
             {
-                Console.Clear();
+                //Console.Clear();
                 string input = Console.ReadLine();
                 if (input == "exit")
                 {
                     break;
                 }
+
+                var projectService = new ProjectService();
+                var projects = projectService.GetAllProjects();
+                foreach (var project in projects)
+                {
+                    Console.WriteLine(project);
+                }
+
             }
         }
     }
