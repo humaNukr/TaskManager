d0beb915c5f81fcc3c741888027c7ad9bc763d46
Artem <artemgritsenko2007@gmail.com>
2026-02-11 17:19:44 +0200

    feat: add init task service and mock storage

---
+++ TaskManager.Services/MockStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using KMA.TaskManager.DataModels;
+
+namespace KMA.TaskManager.Services
+{
+    internal static class MockStorage
+    {
+        private static readonly List<TaskDataModel> _tasks;
+
+
+    }
+}
---
+++ TaskManager.Services/TaskService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using KMA.TaskManager.DataModels;
+using KMA.TaskManager.UIModels;
+
+namespace KMA.TaskManager.Services
+{
+    public class TaskService
+    {
+        public TaskUIModel MapToUI(TaskDataModel data)
+        {
+            return data is null
+                ? throw new ArgumentNullException(nameof(data))
+                : new TaskUIModel(
+                    data.Id,
+                    data.ProjectId,
+                    data.Name,
+                    data.Description,
+                    data.Priority,
+                    data.DueDate,
+                    data.IsCompleted
+                );
+        }
+    }
+}
