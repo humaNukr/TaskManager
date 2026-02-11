80c13e3fedb184c8c479be76c90cf28484356ca5
DenisKoriavets <d.korjavets@ukr.net>
2026-02-11 19:37:49 +0200

    feat: implement main application loop and exit logic

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                string input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    break;
+                }
+            }
         }
     }
 }
