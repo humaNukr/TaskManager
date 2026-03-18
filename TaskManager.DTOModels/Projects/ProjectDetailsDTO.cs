8d39db98c10d7acde1351746ce7a9f28bebbad16
DenisKoriavets <d.korjavets@ukr.net>
2026-03-18 23:36:37 +0200

    Add ProjectDetailsDTO and ProjectListDTO records

---
+++ TaskManager.DTOModels/Projects/ProjectDetailsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using KMA.TaskManager.Common.Enums;
+using KMA.TaskManager.Services.DTOModels.Tasks;
+
+namespace KMA.TaskManager.Services.DTOModels.Projects;
+
+public record ProjectDetailsDTO(
+    Guid Id,
+    string Name,
+    string Description,
+    ProjectType ProjectType,
+    IEnumerable<TaskListDto> Tasks
+);
---
+++ TaskManager.DTOModels/Projects/ProjectListDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KMA.TaskManager.Services.DTOModels.Projects;
+public record ProjectListDTO(
+    Guid Id,
+    string Name,
+    int TotalTasks,
+    int CompletedTasks
+);
