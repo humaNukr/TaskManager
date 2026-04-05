be698c39c920e55f6e0d0b0b52f612650879e4fc
DenisKoriavets <d.korjavets@ukr.net>
2026-04-05 11:51:16 +0300

    refactor(projects): switch DTOs to classes, fix setters and progress DRY

--- TaskManager.DTOModels/Projects/ProjectDetailsDTO.cs
+++ TaskManager.DTOModels/Projects/ProjectDetailsDTO.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using KMA.TaskManager.Common.Enums;
 using KMA.TaskManager.Services.DTOModels.Tasks;
 
 namespace KMA.TaskManager.Services.DTOModels.Projects;
 
-public record ProjectDetailsDTO(
-    Guid Id,
-    string Name,
-    string Description,
-    ProjectType ProjectType,
-    IEnumerable<TaskListDTO> Tasks
-)
+public class ProjectDetailsDTO
 {
+    public Guid Id { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public ProjectType ProjectType { get; }
+    public IEnumerable<TaskListDTO> Tasks { get; }
+
+    public ProjectDetailsDTO(
+        Guid id,
+        string name,
+        string description,
+        ProjectType projectType,
+        IEnumerable<TaskListDTO> tasks)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        ProjectType = projectType;
+        Tasks = tasks;
+    }
+
     public int TotalTasks => Tasks?.Count() ?? 0;
     public int CompletedTasks => Tasks?.Count(t => t.IsCompleted) ?? 0;
-
-    public double Progress => TotalTasks == 0 ? 0 : (double)CompletedTasks / TotalTasks * 100;
     public double ProgressFraction => TotalTasks == 0 ? 0 : (double)CompletedTasks / TotalTasks;
+    public double Progress => ProgressFraction * 100;
+
     public string ProgressStats => $"{CompletedTasks} з {TotalTasks} завдань завершено";
 }
--- TaskManager.DTOModels/Projects/ProjectListDTO.cs
+++ TaskManager.DTOModels/Projects/ProjectListDTO.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
 namespace KMA.TaskManager.Services.DTOModels.Projects;
-public record ProjectListDTO(
-    Guid Id,
-    string Name,
-    int TotalTasks,
-    int CompletedTasks
-)
+
+public class ProjectListDTO
 {
+    public Guid Id { get; }
+    public string Name { get; }
+    public int TotalTasks { get; }
+    public int CompletedTasks { get; }
+
+    public ProjectListDTO(
+        Guid id,
+        string name,
+        int totalTasks,
+        int completedTasks)
+    {
+        Id = id;
+        Name = name;
+        TotalTasks = totalTasks;
+        CompletedTasks = completedTasks;
+    }
+
     public double Progress => TotalTasks == 0 ? 0 : (double)CompletedTasks / TotalTasks * 100;
 }
